089920ecda42489daaa72a7f95d85dacbb22d49a
zydplatform <kigandaivan32@gmail.com>
2021-10-27 23:46:42 -0700

    update

--- gs1uganda/app/assets/barcode.js
+++ gs1uganda/app/assets/barcode.js
@@ -6,7 +6,7 @@ $("#barcoderequest").on('click', function (e) {
   var numberofBarcodes = $("#barcodetotal").val();
    $.ajax({
 
-        url: "http://83.136.248.89:1701/gTins/number/numberofBarcodes",
+        url: "http://83.136.248.89:1701/gTins/number/"+numberofBarcodes,
         type: "GET",
         dataType: "json",
         headers: {
@@ -20,4 +20,29 @@ $("#barcoderequest").on('click', function (e) {
 
 });
 
+$.ajax({
+
+        url: "http://83.136.248.89:1701/gTins/loggedinuser",
+        type: "GET",
+        dataType: "json",
+        headers: {
+            "Authorization" : 'Bearer '+localStorage.getItem('myToken')
+        },
+        success: function (result) {
+                  // console.log(JSON.stringify(result.data));
+                    for (var x in result.data) {
+          var dataforbarcodes = result.data[x];
+          $('#tablebarcodes tbody').append($('<tr>')
+              .append($('<td>', { text: dataforbarcodes.gtin }))
+              .append($('<td>', { text: dataforbarcodes.barcodeType }))
+              .append($('<td>', { text: dataforbarcodes.activationStatus }))
+              // .append($('<td>', {html:'<button class="btn btn-sm btn-warning">update</button><button class="btn btn-sm btn-danger">delete</button>'}))
+          )
+       }
+          
+        }
+    })
+
 });
+
+
